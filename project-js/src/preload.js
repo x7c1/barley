e3189b2f8e1c2bb1659bf5ca1871e4219793987e
x7c1 <jmtaro+x7c1@gmail.com>
2021-01-03 14:13:17 +0900

    avoid unsafe sample

--- project-js/src/preload.js
+++ project-js/src/preload.js
@@ -1,3 +1,17 @@
 process.once('loaded', () => {
-    global.process = process;
+    /*
+    // it's unsafe to make original `process` public as it is:
+    // (especially when your app opens external sites)
+    global.process = process
+
+    // decorate it instead, with an object which has minimum fields for your app:
+    */
+    global.process = {
+        getSystemVersion() {
+            return process.getSystemVersion()
+        },
+        get type() {
+            return process.type
+        }
+    };
 });
